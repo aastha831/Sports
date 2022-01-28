cefaeedc6a9020921d3efd5dcde6f85be9c50353
aastha831 <aastha.shah@bytestechnolab.in>
2022-01-28 15:18:04 +0530

    test

--- wp-content/themes/twentytwentyone-child/functions.php
+++ wp-content/themes/twentytwentyone-child/functions.php
@@ -6,3 +6,19 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+function create_posttype() {
+register_post_type( 'news',
+// CPT Options
+array(
+  'labels' => array(
+   'name' => __( 'news' ),
+   'singular_name' => __( 'News' )
+  ),
+  'public' => true,
+  'has_archive' => false,
+  'rewrite' => array('slug' => 'news'),
+ )
+);
+}
+// Hooking up our function to theme setup
+add_action( 'init', 'create_posttype' );
